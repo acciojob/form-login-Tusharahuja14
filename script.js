f32d7efe0695779b3eef214bfd96c02405221ab4
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-19 17:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+let fname=document.getElementById("fname");
+	let lname=document.getElementById("lname");
+	alert(`${fname.value} ${lname.value}`);
 }
